39942ac75b1513ac7117daba02e009f49c3279a9
Persegue <leandro.santos@drmonitora.com.br>
2022-10-09 23:08:36 -0300

    Adding files for testing purposes

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+	res.json({ message: "Olá mundo!"  });
+})
+
+app.listen(PORT, () => {
+	console.log("Server Running on PORT", PORT);
+})
